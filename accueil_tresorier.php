bbfa2056d1f27db7b109fd7b4828409a1ee3c8f4
ConseillerDoriantation <37410169+ConseillerDoriantation@users.noreply.github.com>
2019-04-11 09:22:04 +0200

    Create accueil_tresorier.php

---
+++ accueil_tresorier.php
@@ -0,0 +1,26 @@
+<?php
+	include('header.php');
+?>
+
+	</br>
+	</br>	
+<!-- Affichage de la bannière -->
+
+<div class="accueil_avalider">
+	</br>
+	<h1>Frais à valider</h1>
+	</br>
+	<h4> Retrouver dans cette rubrique les frais qu'il vous reste à valider </h4>
+	<a href="#.php" class="btn_index">Accès Frais à valider</a>
+</div>
+
+<div class="accueil_fraisvalide">
+	<br>
+	<h1>Frais Validé</h1>
+	</br>
+		<a href="#.php" class="btn_index">Consulter Frais validé</a>
+</div>
+
+<?php
+	include("footer.html");
+?>
