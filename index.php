824515198449de58879526215970cc3e6c5ffd2c
Jean <49511951+Charpente@users.noreply.github.com>
2019-04-12 11:57:20 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 ?>
 
 <head>
-	<title> Pages du Stagiaire </title>
+	<title> M2L_CONNEXION </title>
 
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
