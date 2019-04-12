30234d24103cb51abf9542efc8d21ec4372d6ab8
Jean <49511951+Charpente@users.noreply.github.com>
2019-04-12 11:56:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
 <?php
 	$_SESSION['connecte'] = "";
-	include('header.php');
 ?>
+
+<head>
+	<title> Pages du Stagiaire </title>
+
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" media="screen" type="text/css" title="Design" href="style.css" />
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+</head>
 
 <script type="text/javascript">
 	function verifChamps()
