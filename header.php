a0766e6a566d7ea7274d5d485fa13e4f01f951f7
ConseillerDoriantation <37410169+ConseillerDoriantation@users.noreply.github.com>
2019-05-29 12:06:23 +0200

    Update header.php

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title> Pages du Stagiaire </title>
+	<title> M2L </title>
 
 	<meta charset="utf-8" />
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
