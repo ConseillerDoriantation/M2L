6d90ba5574ec6003c2df93ae723e650483a52fc6
ConseillerDoriantation <37410169+ConseillerDoriantation@users.noreply.github.com>
2019-06-01 19:23:52 +0200

    Update inscriptionBdd.php

--- inscriptionBdd.php
+++ inscriptionBdd.php
@@ -1,5 +1,6 @@
 <?php 
 	ob_start();
+	require("connexionBddID.php");
 
 	// On récupère les données saisies dans le formulaire
 	$Identifiant = $_POST["adrMel"];
@@ -9,7 +10,6 @@
 	$rue = $_POST["rue"];
 	$cp = $_POST["cp"];
 	$mdp = $_POST["mdp"];
-	$confirmMdp = $_POST["mdpConfirm"];
 
 	$sqlVerifMdp = ("SELECT MAIL_DEM FROM demandeurs");
 	$test = $connexion->query($sqlVerifMdp) or die ("Erreur dans la requ&ecircte sql");
@@ -21,24 +21,32 @@
 	$ligne = $numId->fetch();
 	$numero = $ligne[0]+1;
 
-	if (($mdp == $confirmMdp) && ($Identifiant != $VerifMdp)){
-		$reqSQL =$connexion->prepare('INSERT INTO demandeurs(MAIL_DEM, NOM_DEM, PRENOM_DEM, RUE_DEM, CP_DEM, VILLE_DEM, NUM_DEM, MDP_DEM) 
-			VALUES(:id, :nom, :pre, :rue, :cp, :ville, :num , :mdp)');
-		$reqSQL -> execute(array(
-			'id' => $Identifiant, 
-			'nom' => $nom, 
-			'pre' => $prenom, 
-			'rue' => $rue, 
-			'cp' => $cp,
-			'ville' => $ville, 
-			'num' => $numero, 
-			'mdp' => $mdp));
-		echo "$Identifiant, $nom, $prenom, $rue, $cp, $ville, $numero, $mdp";
-		//include("index.php");
-	}
+	if ($Identifiant != $VerifMdp){
 
-	else{
-		$messageErreur = "erreur, mot de passe ou identifiant incorrect";
-		echo "<script type='text/javascript'>alert('$messageErreur');</script>";
+		// On vérifie que l'adresse n'existe pas dans la BDD
+		$sql = ("SELECT COUNT(MAIL_DEM) as mail_exists FROM DEMANDEURS WHERE MAIL_DEM = '".$Identifiant."'");
+		$result = $connexion->query($sql) or die ("Erreur dans la requ&ecircte sql");
+		$mailexists = $result->fetch();
+		if($mailexists['mail_exists'] > 0)
+		{
+			header('Location: inscription.php?msg=2 ');
+		}
+		else
+		{
+			echo "samarch";
+			$reqSQL =$connexion->prepare('INSERT INTO demandeurs(MAIL_DEM, NOM_DEM, PRENOM_DEM, RUE_DEM, CP_DEM, VILLE_DEM, NUM_DEM, MDP_DEM) 
+				VALUES(:id, :nom, :pre, :rue, :cp, :ville, :num , :mdp)');
+			$reqSQL -> execute(array(
+				'id' => $Identifiant, 
+				'nom' => $nom, 
+				'pre' => $prenom, 
+				'rue' => $rue, 
+				'cp' => $cp,
+				'ville' => $ville, 
+				'num' => $numero, 
+				'mdp' => $mdp));
+			echo "$Identifiant, $nom, $prenom, $rue, $cp, $ville, $numero, $mdp";
+			header('Location: index.php?msg=1');
+		}
 	}
 ?>
