9a0151022c9a0339992b08fff0b8000b83ad018d
Jean <49511951+Charpente@users.noreply.github.com>
2019-06-02 21:19:56 +0200

    Update connexionBddID.php

--- connexionBddID.php
+++ connexionBddID.php
@@ -1,14 +1,14 @@
 <?php
 	// Définitions de constantes pour la connexion à MySQL
-	/*$SERVEUR = 'mledfjbodqbddm2l.mysql.db'; 
-	$BASE = 'mledfjbodqbddm2l';
-	$NOM = 'mledfjbodqbddm2l';
-	$MOTPASSE = '12f4qds90iY';*/
-
-	$SERVEUR = 'localhost'; 
+	/*$SERVEUR = 'localhost'; 
 	$BASE = 'bddm2l';
 	$NOM = 'root';
-	$MOTPASSE = '';
+	$MOTPASSE = '';*/
+
+	$SERVEUR = 'mledfjbodqbddm2l.mysql.db'; 
+	$BASE = 'mledfjbodqbddm2l';
+	$NOM = 'mledfjbodqbddm2l';
+	$MOTPASSE = '12f4qds90iY';
 
 
 	// Connexion à la base de données
