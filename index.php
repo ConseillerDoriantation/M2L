eba6fd02cc41e8e403888061d83aa13ce37cc2a2
ConseillerDoriantation <37410169+ConseillerDoriantation@users.noreply.github.com>
2019-06-02 18:56:42 +0200

    Update index.php

--- index.php
+++ index.php
@@ -18,6 +18,7 @@
 <div id="wrapper">
 	<div class="row justify-content-center">
 		<div class="card col col-sm-10 col-md-6 col-lg-5 col-xl-3" style="margin-top : 200px;">
+			<!-- Card de connexion -->
 			<div class="card-body">
 				<h4 class="card-title mb-4">Connexion</h4>
 				<?php
@@ -32,27 +33,28 @@
 						}
 					}
 				?>
+				<!--formulaire -->
 				<div class="form-body">
-					<form method="post" action="connexionBdd.php" name="identification" onSubmit="return verifChamps()">
+					<form method="post" action="connexionBdd.php" name="identification">
 						<div class="input-group flex-nowrap">
 							<div class="input-group-prepend">
 								<span class="input-group-text"  style="width : 125px;" id="addon-wrapping">Identifiant</span>
 							</div>
-							<input id="identifiant" name="identifiant" type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping">
+							<input id="identifiant" name="identifiant" type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="addon-wrapping" required>
 						</div>
 
 						<div class="mt-2 input-group mb-3">
 							<div class="input-group-prepend">
 								<span class="input-group-text" style="width : 125px;" id="inputGroup-sizing-default">Mot de passe</span>
 							</div>
-							<input id="mdp" name="mdp" type="password" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+							<input id="mdp" name="mdp" type="password" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" required>
 						</div>
 
 						<input class="mr-4 btn btn-info" type="submit" name="seConnecter" Value="Connexion"/>
 						<a class="btn btn-success" href="inscription.php">Inscription</a>
 					</form>
-				</div>
-			</div>
+				</div> <!-- fin formulaire -->
+			</div> <!-- fin card -->
 		</div>
 	</div>
 </div> <!-- end wrapper -->
