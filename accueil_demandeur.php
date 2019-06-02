1947f912ec919698e9f174a64a95704fb9901efb
ConseillerDoriantation <37410169+ConseillerDoriantation@users.noreply.github.com>
2019-06-02 19:18:41 +0200

    Update accueil_demandeur.php

--- accueil_demandeur.php
+++ accueil_demandeur.php
@@ -5,71 +5,61 @@
 	}
 ?>
 
-<!-- Affichage de la bannière -->
 <div id="wrapper">
-	<div>
-		<div class="row justify-content-center" id="logo">
-			<img src="images/logo.png"/>
-		</div>
-		<!--SLIDE D IMAGE-->
-		<div class="sliberbox">
-				<div id="slider">
-					<div id="box">
-						<img class='img1' src="images/a.jpg">
-					</div>
-
-					<label class="prew" onclick="prewImage()">  </label>
-					<label class="next" onclick="nextImage()">  </label>
-				</div>
-			</div>
-			<!--JAVA Fontions d affichage des images Suivante et Precédente--> 
-			<script type="text/javascript">
-				var slider_content = document.getElementById("box");
-				var image = ["images/a","images/b","images/c"];
-
-				var i= image.length;
-
-				function nextImage(){
-					if (i<image.length){
-						i=i+1;
-					}else{
-						i = 1;
-					}
-					slider_content.innerHTML = "<img class='img1' src="+image[i-1]+".jpg >";
-				}
-
-				function prewImage(){
-					if (i<image.length+1 && i>1){
-						i=i-1;
-					}else{
-						i = image.length;
-					}
-					slider_content.innerHTML = "<img class='img1' src="+image[i-1]+".jpg>";
-				}
-
-				setInterval(nextImage , 6000);
-			</script>
-		<div class="container">
-			<div class="row">
-				<div class="col-sm">
-					<div class="divimg">
-						<a class="" href="nouveau_frais.php" title="Nouvelle ligne de frais">
-							<span class="hover">
-								VISIT WEBSITE <i class="icon ion-md-log-out"></i>
-							</span>
-							<img class="img-fluid" src="/uploads/design/2013-09/axelboberg-se-8773-2.png" alt="">
-						</a>
-					</div>
-				</div>
-				<div class="col-sm">
-					One of three columns
-				</div>
-				<div class="col-sm">
-					One of three columns
-				</div>
-			</div>
-		</div>
+	<!-- Logo -->
+	<div class="row justify-content-center" id="logo">
+		<img src="images/logo.png"/>
 	</div>
+	<!-- Caroussel -->
+	<div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+		<ol class="carousel-indicators">
+			<li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+		</ol>
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+			<img src="images/a.jpg" class="d-block w-100" alt="...">
+			</div>
+			<div class="carousel-item">
+			<img src="images/b.jpg" class="d-block w-100" alt="...">
+			</div>
+			<div class="carousel-item">
+			<img src="images/c.jpg" class="d-block w-100" alt="...">
+			</div>
+		</div>
+		<a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Previous</span>
+		</a>
+		<a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Next</span>
+		</a>
+	</div> <!-- Fin Caroussel -->
+
+	<!-- Lien -->
+	<div class="row">
+		<div class="col-sm-6">
+			<div class="card">
+			<div class="card-body">
+				<h5 class="card-title">Special title treatment</h5>
+				<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+				<a href="#" class="btn btn-primary">Go somewhere</a>
+			</div>
+			</div>
+		</div>
+		<div class="col-sm-6">
+			<div class="card">
+			<div class="card-body">
+				<h5 class="card-title">Special title treatment</h5>
+				<p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+				<a href="#" class="btn btn-primary">Go somewhere</a>
+			</div>
+			</div>
+		</div>
+	</div><!-- Fin lien -->
+	
 	<a href="nouveau_frais.php" style="background-color: #A6AFAE;" class="btn_index">Nouveau Frais</a>
 
 	<a href="liste_frais.php" class="btn_index">Consulter Frais</a>
